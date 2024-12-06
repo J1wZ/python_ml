fe1267474533a75f5f0e5cd37e77869550aabf64
J1wZ <93642863+J1wZ@users.noreply.github.com>
2024-12-06 16:51:46 +0400

    Добавлено Задание4

---
+++ дз-04-12/Задание4.py
@@ -0,0 +1,20 @@
+# Задача 4 Комбинации символов 
+# Решить следующее задание сприменением библиотеки itertools.
+# Реализовать функцию get_combinations. 
+# Должна принимать строку s и число k и возвращать 
+# все возможные комбинации из символов в строке s 
+# с длинами <= k (использовать itertools.combinations)
+
+from itertools import combinations
+
+def get_combinations(s : str, k : int):
+    result = []
+    for i in range(k+1):
+        result += list(combinations(s, i))
+    return result
+
+string = 'Hello_World!'
+num = 3
+result = get_combinations(string, num)
+for el in result:
+    print(el)
