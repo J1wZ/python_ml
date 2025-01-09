407bc06bd0289bb1446840bdefca5d2d7e5cec38
J1wZ <93642863+J1wZ@users.noreply.github.com>
2025-01-09 22:14:40 +0300

    PySpark1_1

---
+++ дз-23-12/PySpark-Задание1_1.py
@@ -0,0 +1,25 @@
+import findspark
+findspark.init()
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("local").getOrCreate()
+
+#API
+df_brands = spark.read.csv("CVS\\brands.csv", inferSchema=True, header=True, sep=",")
+
+df_products = spark.read.csv("CVS\\products.csv", inferSchema=True, header=True, sep=",")
+
+df_res = df_products.join(df_brands, df_brands["brand_id"] == df_products["brand_id"])
+df_res = df_res.select("product_name", "brand_name")
+df_res.show()
+
+#spark.sql
+df_brands.createOrReplaceTempView("brands")
+df_products.createOrReplaceTempView("products")
+
+spark.sql("""
+          SELECT products.product_name, brands.brand_name 
+            FROM products 
+            JOIN brands ON brands.brand_id = products.brand_id
+          """).show()
