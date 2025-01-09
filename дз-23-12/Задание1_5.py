e66f5afc10998ff77625106ece844507565242cd
J1wZ <93642863+J1wZ@users.noreply.github.com>
2025-01-09 16:49:08 +0300

    Добавлено Задание1_5

---
+++ дз-23-12/Задание1_5.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, text
+ 
+user = 'root'
+password = 'NoMorFort19#'
+host = '127.0.0.1'
+port = 3306
+database = 'bikestore'
+
+def get_connection():
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(user, password, host, port, database)
+    )
+
+
+if __name__ == '__main__':
+    try:
+        engine = get_connection()
+        print(f"Успешно подключено.")
+        with engine.connect() as connection:
+            query="""
+            SELECT orders.customer_id, COUNT(orders.order_id) as count
+            FROM orders
+            GROUP BY orders.customer_id;"""
+            my_data=list(connection.execute(text(query)))
+
+        for el in my_data:
+            print(el)
+    except Exception as ex:
+        print("Не получилось подключиться: \n", ex)
