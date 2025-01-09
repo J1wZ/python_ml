107339d7c178f240376a78f2cecf9a56ef6d6ba5
J1wZ <93642863+J1wZ@users.noreply.github.com>
2025-01-09 17:03:13 +0300

    Задание1_6

---
+++ дз-23-12/Задание1_6.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine, text
+ 
+user = 'root'
+password = 'NoMorFort19#'
+host = '127.0.0.1'
+port = 3306
+database = 'bikestore'
+
+def get_connection():
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(user, password, host, port, database)
+    )
+
+
+if __name__ == '__main__':
+    try:
+        engine = get_connection()
+        print(f"Успешно подключено.")
+        with engine.connect() as connection:
+            query="""
+            SELECT CONCAT_WS(' ',customers.first_name, customers.last_name) as customer, COUNT(orders.order_id) as order_count
+            FROM customers
+            JOIN orders ON orders.customer_id = customers.customer_id
+            GROUP BY customer
+            HAVING order_count > 0;"""
+            my_data=list(connection.execute(text(query)))
+
+        for el in my_data:
+            print(el)
+    except Exception as ex:
+        print("Не получилось подключиться: \n", ex)
