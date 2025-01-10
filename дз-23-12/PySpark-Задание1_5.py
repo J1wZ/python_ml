d282763e55bb64950d92946ac166b9f03e44aed9
J1wZ <93642863+J1wZ@users.noreply.github.com>
2025-01-10 11:14:47 +0300

    PySpark1_5

--- дз-23-12/PySpark-Задание1_5.py
+++ дз-23-12/PySpark-Задание1_5.py
@@ -4,34 +4,29 @@ from pyspark.sql.functions import concat_ws
 spark = SparkSession.builder.master("local").getOrCreate()
 
 #API
-
 df_customers = spark.read.csv("CVS\\customers.csv", inferSchema=True, header=True, sep=",")
 df_orders = spark.read.csv("CVS\\orders.csv", inferSchema=True, header=True, sep=",")
-df_stores = spark.read.csv("CVS\\stores.csv", inferSchema=True, header=True, sep=",")
 
 df_res = df_customers.join(df_orders, 
                            df_orders["customer_id"] == df_customers["customer_id"])
 
-df_res = df_res.join(df_stores, 
-                     df_stores["store_id"] == df_orders["store_id"]
-                     ).where(df_stores["store_name"] == 'Rowlett Bikes')
-
 df_res = df_res.select(
     concat_ws(" ",df_customers["first_name"], 
-              df_customers["last_name"]).alias("customer"),
-              df_customers["email"], df_customers["phone"])
+              df_customers["last_name"]).alias("customer"))
+df_res = df_res.groupBy("customer").count()
 
-df_res.dropDuplicates().show()
-
+df_res.show()
 #spark.sql
+df_customers = spark.read.csv("CVS\\customers.csv", inferSchema=True, header=True, sep=",")
+df_orders = spark.read.csv("CVS\\orders.csv", inferSchema=True, header=True, sep=",")
+
+
 df_customers.createOrReplaceTempView("customers")
-df_stores.createOrReplaceTempView("stores")
 df_orders.createOrReplaceTempView("orders")
 
 spark.sql("""
-          SELECT CONCAT_WS(' ',customers.first_name, customers.last_name) as customer, customers.email, customers.phone
+            SELECT CONCAT_WS(' ',customers.first_name, customers.last_name) as customer, COUNT(orders.order_id) as count
             FROM customers
             JOIN orders ON orders.customer_id = customers.customer_id
-            JOIN stores ON stores.store_id = orders.store_id
-            WHERE stores.store_name = 'Rowlett Bikes'
-          """).dropDuplicates().show()
+            GROUP BY customer;
+          """).show()
